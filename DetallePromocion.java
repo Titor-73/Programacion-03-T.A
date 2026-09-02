53dbf8b6b1b12a13e17781ffc3406853aa09cfe9
gabrielcq6225 <a20236225@pucp.edu.pe>
2026-09-01 23:02:09 -0500

    Update DetallePromocion.java

--- DetallePromocion.java
+++ DetallePromocion.java
@@ -18,6 +18,6 @@ public class DetallePromocion {
 	public int getIdDetallePromocion() { return idDetallePromocion; }
     public TipoDescuento getTipoDescuento() { return tipoDescuento; }
     public BigDecimal getValorDescuento() { return valorDescuento; }
-    public Promocion getPromocion() { return new Promocion(promocion); }
-    public Producto getProducto() { return new Producto(producto); }
+    public Promocion getPromocion() { return (promocion != null) ? new Promocion(promocion) : null; }
+    public Producto getProducto() { return (producto != null) ? new Producto(producto) : null; }
 }
