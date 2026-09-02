ccb92214282575d0dcd136f057b37e215a63c581
gabrielcq6225 <a20236225@pucp.edu.pe>
2026-09-01 23:02:18 -0500

    Update DetallePedido.java

--- DetallePedido.java
+++ DetallePedido.java
@@ -12,10 +12,10 @@ public class DetallePedido {
 	public DetallePedido(int idDetallePedido, int cantidad, BigDecimal precioReferencial, BigDecimal descuentoReferencial, Pedido pedido, Producto producto) {
         this.idDetallePedido = idDetallePedido;
         this.cantidad = cantidad;
-        this.precioReferencial = precioReferencial;
+        this.precioReferencial = (precioReferencial != null) ? precioReferencial : BigDecimal.ZERO;
         this.descuentoReferencial = (descuentoReferencial != null) ? descuentoReferencial : BigDecimal.ZERO;
-        this.pedido = new Pedido(pedido);
-        this.producto = new Producto(producto);
+        this.pedido = (pedido != null) ? new Pedido(pedido) : null;
+        this.producto = (producto != null) ? new Producto(producto) : null;
         this.calcularSubtotalReferencial();
     }
 	
@@ -24,8 +24,8 @@ public class DetallePedido {
     public BigDecimal getPrecioReferencial() { return precioReferencial; }
     public BigDecimal getDescuentoReferencial() { return descuentoReferencial; }
     public BigDecimal getSubtotalReferencial() { return subtotalReferencial; }
-    public Pedido getPedido() { return new Pedido(this.pedido); }
-    public Producto getProducto() { return new Producto(this.producto); }
+    public Pedido getPedido() { return (pedido != null) ? new Pedido(pedido) : null; }
+    public Producto getProducto() { return (producto != null) ? new Producto(producto) : null; }
 	
 	public void calcularSubtotalReferencial() {
         BigDecimal precioEfectivo = this.precioReferencial.subtract(this.descuentoReferencial);
