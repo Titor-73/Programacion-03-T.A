2af1478e4c20f807f6899036d4c71bef941dcb0d
sergioMDC05 <srurikmedina@gmail.com>
2026-09-13 22:59:12 -0500

    Update TipoMovimientoInventario.java

--- pazcompras_domain/src/main/java/pe/edu/pucp/pazcompras/inventario/TipoMovimientoInventario.java
+++ pazcompras_domain/src/main/java/pe/edu/pucp/pazcompras/inventario/TipoMovimientoInventario.java
@@ -1,4 +1,4 @@
-package pe.edu.pucp.pazcompras.inventario.model;
+package pe.edu.pucp.pazcompras.inventario;
 
 public enum TipoMovimientoInventario {
     INGRESO, VENTA, AJUSTE_POSITIVO, AJUSTE_NEGATIVO, RESERVA, LIBERACION_RESERVA, MERMA, VENCIMIENTO
