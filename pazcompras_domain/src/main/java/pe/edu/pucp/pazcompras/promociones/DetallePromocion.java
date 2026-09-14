544d936d624d746bd042dda7171be8159321bc22
sergioMDC05 <srurikmedina@gmail.com>
2026-09-13 23:03:09 -0500

    Move DetallePromocion class to new package

--- pazcompras_domain/src/main/java/pe/edu/pucp/pazcompras/promociones/DetallePromocion.java
+++ pazcompras_domain/src/main/java/pe/edu/pucp/pazcompras/promociones/DetallePromocion.java
@@ -1,6 +1,6 @@
-package pe.edu.pucp.pazcompras.promociones.model;
+package pe.edu.pucp.pazcompras.promociones;
 
-import pe.edu.pucp.pazcompras.catalogo.model.Producto;
+import pe.edu.pucp.pazcompras.catalogo.Producto;
 
 import java.math.BigDecimal;
 
